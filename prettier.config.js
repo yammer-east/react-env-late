5a9b8e23d46c758738174993a0ad1925ba2b7c3d
yammer9 <95598958+yammer-east@users.noreply.github.com>
2024-01-14 17:30:14 +0900

    init

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+/** @type {import("prettier".Config)} */
+const config = {
+  semi: true,
+};
+
+export default config;
---
+++ src/components/TextInput.test.tsx
@@ -0,0 +1,14 @@
+import userEvent from "@testing-library/user-event";
+import TextInput from "./TextInput";
+import { render, screen } from "@testing-library/react";
+test("TextInput Component Test", async () => {
+  const user = userEvent.setup();
+  render(<TextInput />);
+
+  const textElement = screen.getByText("Entered Text:");
+  expect(textElement).toBeInTheDocument();
+
+  const inputElement = screen.getByLabelText("Text Input");
+  await user.type(inputElement, "hello world");
+  expect(screen.getByText("Entered Text: hello world")).toBeInTheDocument();
+});
