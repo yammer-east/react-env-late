98dfa6ea31aacefcbeae62357e9b55a0b85d3ea5
yammer9 <95598958+yammer-east@users.noreply.github.com>
2024-01-14 17:56:43 +0900

    test2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
+    "plugin:storybook/recommended"
   ],
   overrides: [
     {
@@ -28,4 +29,7 @@ module.exports = {
   rules: {
     "react/react-in-jsx-scope": "off",
   },
+  settings: {
+    react: {version: 'detect'},
+  }
 };
